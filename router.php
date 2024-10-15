1c3c8be928ef6e89f4c3dd0825513e9174df84f5
Matias Morcillo <143836035+MatiasMor64@users.noreply.github.com>
2024-10-15 11:22:36 -0300

    router rudimentario (+ htaccess)
    se agrega una version extremadamente rudimentaria del router con su tabla de ruteo para comenzar a trabajar, tambien se agrega el .htaccess y una imagen para la pagina

---
+++ router.php
@@ -0,0 +1,88 @@
+<?php
+require_once 'noticias.php';
+require_once 'about.php';
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+$action = 'home';
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+/* TABLA DE RUTEO:
+inicio/home         ->  showHome; (muestra todo a la vez)
+juego especifico    ->  showJuego; (muestra un juego en especifico)
+mostrar lista plata ->
+mostrar lista cate  ->
+login               ->
+logout              ->
+crear usuario       ->
+crear juego         ->
+crear categoria     ->               
+crear plataforma    ->               
+borrar juego        ->
+borrar categoria    ->
+borrar plataforma   ->
+modificar juego     ->
+modificar categoria ->
+modificar plataforma->
+*/
+
+$params = explode('/', $action);
+    
+switch ($params[0]) {
+    case 'home':
+        showHome();
+        break;
+    case 'juego':
+        if (isset($params[1])){
+            $id= $params[1];
+            showJuego($params[1]);
+        }
+        break;
+    case 'listaPlataforma':
+        
+        break;
+    case 'listaGenero':
+        
+        break;
+    case 'login':
+    
+        break;
+    case 'logout':
+    
+        break;
+    case 'nuevoUser':
+    
+        break;
+    case 'nuevoJuego':
+    
+        break;
+    case 'nuevaCat':
+    
+        break;
+    case 'nuevaPlat':
+    
+        break;
+    case 'borrarJuego':
+    
+        break;
+    case 'borrarCat':
+
+        break;
+    case 'borrarPlat':
+        
+        break;
+    case 'modifJuego':
+        
+        break;
+    case 'modifCat':
+        
+        break;
+    case 'modifPlat':
+        
+        break;
+    default:
+        echo "404 not found";
+        break;
+}
